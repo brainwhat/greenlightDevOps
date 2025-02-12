d88f3020c3f9b193547bc0d9ebc16c424128bcfd
brainwhat <facelessbeach@gmail.com>
2025-02-12 23:16:40 +0300

    added httprouter package and basic handlers

---
+++ cmd/api/movies.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Creating a movie...")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	id, err := app.readIDParams(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Looking for movie №%d\n", id)
+}
