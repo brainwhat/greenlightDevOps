82d2087eea04d05be3a22c75fa639d59f0bea34a
brainwhat <facelessbeach@gmail.com>
2025-02-13 23:20:04 +0300

    added Movie struct that marshals into json response

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"greenlight.brainwhat/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,5 +20,19 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "Looking for movie №%d\n", id)
+	// We didn't set the value of Runtime. It'll be set to zero by json.Marshal
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Titanic",
+		Year:      0,
+		Genres:    []string{"sexy dicaprio", "drama", "tragedy"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
---
+++ internal/data/movies.go
@@ -0,0 +1,13 @@
+package data
+
+import "time"
+
+type Movie struct {
+	ID        int64     `json:"id"` // This just changes output names
+	CreatedAt time.Time `json:"-"`  // "-" doen't show field in json response
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"` // omitempty doesn't show field if it's not defined/zero/""/false/etc
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
+}
