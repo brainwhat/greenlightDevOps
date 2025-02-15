1d19d6a6888e826fa9214ab262281a8d36336218
brainwhat <facelessbeach@gmail.com>
2025-02-15 22:05:04 +0300

    added json validation with validator package

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -45,3 +45,7 @@ func (app *application) methodNotAllowedError(w http.ResponseWriter, r *http.Req
 func (app *application) badRequestReponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"greenlight.brainwhat/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"` // This just changes output names
@@ -10,4 +14,19 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "title cannot be empty")
+	v.Check(len(movie.Title) < 500, "title", "title must be under 500 characters")
+
+	v.Check(movie.Year != 0, "year", "must provide year")
+	v.Check(movie.Year > 1888 && movie.Year <= int32(time.Now().Year()), "year", "incorrect year")
+
+	v.Check(movie.Runtime > 0, "runtime", "runtime must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "genres cannot be empty")
+	v.Check(validator.CheckForEmptyStrings(movie.Genres), "genres", "genres cannot be empty")
+	v.Check(len(movie.Genres) > 0 && len(movie.Genres) <= 5, "genres", "movie must have between 1 and 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "genres must be unique")
 }
